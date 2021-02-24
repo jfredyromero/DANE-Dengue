8f723a0bfedd77925364b6e093fe0ef493a4aec8
jfredyromero <66575625+jfredyromero@users.noreply.github.com>
2021-02-24 09:56:11 -0500

    Minimal changes. Name of column chanaged

--- elevation_var.py
+++ elevation_var.py
@@ -10,6 +10,9 @@ elevation_dataframe = pd.read_csv(elevation_file, usecols=['altitude_MEAN'])
 
 # Combino los dataframes por indice
 municipality_dataframe = municipality_dataframe.join(elevation_dataframe)
+# Cambio el nombre de la columna
+municipality_dataframe.rename(columns={'altitude_MEAN':'Altitude Mean (m)'}, inplace=True)
+municipality_dataframe['Altitude Mean (m)'] = municipality_dataframe['Altitude Mean (m)'].str.replace(",", ".").astype(float)
+municipality_dataframe['Altitude Mean (m)'] = pd.to_numeric(municipality_dataframe['Altitude Mean (m)'], errors='coerce')
 # Creating the csv file
 municipality_dataframe.to_csv('DANE_Dengue_Data_Variables.csv', index=False)
-print(municipality_dataframe)
